46d949465745544332b570637652471b4c4b78d9
Hiroaki Nakamura <hnakamur@gmail.com>
2017-10-22 22:13:42 +0900

    Initial commit

---
+++ DemoApp.cpp
@@ -0,0 +1,361 @@
+﻿// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Copyright (c) Microsoft Corporation. All rights reserved
+
+#include "DemoApp.h"
+
+// Provides the application entry point.
+int WINAPI WinMain(
+    HINSTANCE /* hInstance */,
+    HINSTANCE /* hPrevInstance */,
+    LPSTR /* lpCmdLine */,
+    int /* nCmdShow */
+    )
+{
+    // Use HeapSetInformation to specify that the process should
+    // terminate if the heap manager detects an error in any heap used
+    // by the process.
+    // The return value is ignored, because we want to continue running in the
+    // unlikely event that HeapSetInformation fails.
+    HeapSetInformation(NULL, HeapEnableTerminationOnCorruption, NULL, 0);
+
+    if (SUCCEEDED(CoInitialize(NULL)))
+    {
+        {
+            DemoApp app;
+
+            if (SUCCEEDED(app.Initialize()))
+            {
+                app.RunMessageLoop();
+            }
+        }
+        CoUninitialize();
+    }
+
+    return 0;
+}
+
+// DemoApp constructor
+DemoApp::DemoApp() :
+    m_hwnd(NULL),
+	m_pRenderTarget(NULL),
+	m_pDirect2dFactory(NULL),
+	m_pDWriteFactory(NULL),
+	m_pTextFormat(NULL),
+	m_pBlackBrush(NULL)
+{
+}
+
+// DemoApp destructor
+// Releases the application's resources.
+DemoApp::~DemoApp()
+{
+    SafeRelease(&m_pDirect2dFactory);
+	SafeRelease(&m_pDWriteFactory);
+	SafeRelease(&m_pRenderTarget);
+	SafeRelease(&m_pTextFormat);
+	SafeRelease(&m_pBlackBrush);
+
+}
+
+// Creates the application window and device-independent
+// resources.
+HRESULT DemoApp::Initialize()
+{
+    HRESULT hr;
+
+    // Initialize device-indpendent resources, such
+    // as the Direct2D factory.
+    hr = CreateDeviceIndependentResources();
+
+    if (SUCCEEDED(hr))
+    {
+        // Register the window class.
+        WNDCLASSEX wcex = { sizeof(WNDCLASSEX) };
+        wcex.style         = CS_HREDRAW | CS_VREDRAW;
+        wcex.lpfnWndProc   = DemoApp::WndProc;
+        wcex.cbClsExtra    = 0;
+        wcex.cbWndExtra    = sizeof(LONG_PTR);
+        wcex.hInstance     = HINST_THISCOMPONENT;
+        wcex.hbrBackground = NULL;
+        wcex.lpszMenuName  = NULL;
+        wcex.hCursor       = LoadCursor(NULL, IDI_APPLICATION);
+        wcex.lpszClassName = L"D2DDemoApp";
+
+        RegisterClassEx(&wcex);
+
+
+        // Because the CreateWindow function takes its size in pixels,
+        // obtain the system DPI and use it to scale the window size.
+        FLOAT dpiX, dpiY;
+
+        // The factory returns the current system DPI. This is also the value it will use
+        // to create its own windows.
+        m_pDirect2dFactory->GetDesktopDpi(&dpiX, &dpiY);
+
+
+        // Create the window.
+        m_hwnd = CreateWindow(
+            L"D2DDemoApp",
+            L"Direct2D Demo App",
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            static_cast<UINT>(ceil(640.f * dpiX / 96.f)),
+            static_cast<UINT>(ceil(480.f * dpiY / 96.f)),
+            NULL,
+            NULL,
+            HINST_THISCOMPONENT,
+            this
+            );
+        hr = m_hwnd ? S_OK : E_FAIL;
+        if (SUCCEEDED(hr))
+        {
+            ShowWindow(m_hwnd, SW_SHOWNORMAL);
+            UpdateWindow(m_hwnd);
+        }
+    }
+
+    return hr;
+}
+
+// Creates resources that are not bound to a particular device.
+// Their lifetime effectively extends for the duration of the
+// application.
+HRESULT DemoApp::CreateDeviceIndependentResources()
+{
+    HRESULT hr = S_OK;
+
+    // Create a Direct2D factory.
+    hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &m_pDirect2dFactory);
+
+    return hr;
+}
+
+
+// Creates resources that are bound to a particular
+// Direct3D device. These resources need to be recreated
+// if the Direct3D device dissapears, such as when the display
+// changes, the window is remoted, etc.
+HRESULT DemoApp::CreateDeviceResources()
+{
+    HRESULT hr = S_OK;
+
+    if (!m_pRenderTarget)
+    {
+        RECT rc;
+        GetClientRect(m_hwnd, &rc);
+
+        D2D1_SIZE_U size = D2D1::SizeU(
+            rc.right - rc.left,
+            rc.bottom - rc.top
+            );
+
+        // Create a Direct2D render target.
+        hr = m_pDirect2dFactory->CreateHwndRenderTarget(
+            D2D1::RenderTargetProperties(),
+            D2D1::HwndRenderTargetProperties(m_hwnd, size),
+            &m_pRenderTarget
+            );
+
+
+        if (SUCCEEDED(hr))
+        {
+            // Create a black brush.
+            hr = m_pRenderTarget->CreateSolidColorBrush(
+                D2D1::ColorF(D2D1::ColorF::Black),
+                &m_pBlackBrush
+                );
+        }
+		if (SUCCEEDED(hr))
+		{
+
+			// Create a DirectWrite factory.
+			hr = DWriteCreateFactory(
+				DWRITE_FACTORY_TYPE_SHARED,
+				__uuidof(m_pDWriteFactory),
+				reinterpret_cast<IUnknown **>(&m_pDWriteFactory)
+			);
+		}
+		if (SUCCEEDED(hr))
+		{
+			static const WCHAR msc_fontName[] = L"Segoe UI Emoji";
+			static const FLOAT msc_fontSize = 50;
+
+			// Create a DirectWrite text format object.
+			hr = m_pDWriteFactory->CreateTextFormat(
+				msc_fontName,
+				NULL,
+				DWRITE_FONT_WEIGHT_NORMAL,
+				DWRITE_FONT_STYLE_NORMAL,
+				DWRITE_FONT_STRETCH_NORMAL,
+				msc_fontSize,
+				L"", //locale
+				&m_pTextFormat
+			);
+		}
+	}
+
+    return hr;
+}
+
+// Discards device-dependent resources. These resources must be
+// recreated when the Direct3D device is lost.
+void DemoApp::DiscardDeviceResources()
+{
+    SafeRelease(&m_pRenderTarget);
+    SafeRelease(&m_pBlackBrush);
+}
+
+// Runs the main window message loop.
+void DemoApp::RunMessageLoop()
+{
+    MSG msg;
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+}
+
+// This method discards device-specific
+// resources if the Direct3D device dissapears during execution and
+// recreates the resources the next time it's invoked.
+HRESULT DemoApp::OnRender()
+{
+    HRESULT hr = S_OK;
+
+    hr = CreateDeviceResources();
+
+    if (SUCCEEDED(hr))
+    {
+		static const WCHAR sc_helloWorld[] = L"Hello, world! 🙂👍🎉";
+
+		D2D1_SIZE_F rtSize = m_pRenderTarget->GetSize();
+
+		m_pRenderTarget->BeginDraw();
+
+        m_pRenderTarget->SetTransform(D2D1::Matrix3x2F::Identity());
+
+        m_pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::White));
+
+		m_pRenderTarget->DrawText(
+			sc_helloWorld,
+			ARRAYSIZE(sc_helloWorld) - 1,
+			m_pTextFormat,
+			D2D1::RectF(0, 0, rtSize.width, rtSize.height),
+			m_pBlackBrush,
+			D2D1_DRAW_TEXT_OPTIONS_ENABLE_COLOR_FONT
+		);
+
+        hr = m_pRenderTarget->EndDraw();
+    }
+
+    if (hr == D2DERR_RECREATE_TARGET)
+    {
+        hr = S_OK;
+        DiscardDeviceResources();
+    }
+
+    return hr;
+}
+
+
+//  If the application receives a WM_SIZE message, this method
+//  resizes the render target appropriately.
+void DemoApp::OnResize(UINT width, UINT height)
+{
+    if (m_pRenderTarget)
+    {
+        // Note: This method can fail, but it's okay to ignore the
+        // error here, because the error will be returned again
+        // the next time EndDraw is called.
+        m_pRenderTarget->Resize(D2D1::SizeU(width, height));
+    }
+}
+
+// Handles window messages.
+LRESULT CALLBACK DemoApp::WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    LRESULT result = 0;
+
+    if (message == WM_CREATE)
+    {
+        LPCREATESTRUCT pcs = (LPCREATESTRUCT)lParam;
+        DemoApp *pDemoApp = (DemoApp *)pcs->lpCreateParams;
+
+        ::SetWindowLongPtr(
+            hwnd,
+            GWLP_USERDATA,
+#ifdef _WIN64
+			LONG_PTR(pDemoApp)
+#else
+			PtrToUlong(pDemoApp)
+#endif
+            );
+
+        result = 1;
+    }
+    else
+    {
+        DemoApp *pDemoApp = reinterpret_cast<DemoApp *>(static_cast<LONG_PTR>(
+            ::GetWindowLongPtr(
+                hwnd,
+                GWLP_USERDATA
+                )));
+
+        bool wasHandled = false;
+
+        if (pDemoApp)
+        {
+            switch (message)
+            {
+            case WM_SIZE:
+                {
+                    UINT width = LOWORD(lParam);
+                    UINT height = HIWORD(lParam);
+                    pDemoApp->OnResize(width, height);
+                }
+                result = 0;
+                wasHandled = true;
+                break;
+
+            case WM_DISPLAYCHANGE:
+                {
+                    InvalidateRect(hwnd, NULL, FALSE);
+                }
+                result = 0;
+                wasHandled = true;
+                break;
+
+            case WM_PAINT:
+                {
+                    pDemoApp->OnRender();
+                    ValidateRect(hwnd, NULL);
+                }
+                result = 0;
+                wasHandled = true;
+                break;
+
+            case WM_DESTROY:
+                {
+                    PostQuitMessage(0);
+                }
+                result = 1;
+                wasHandled = true;
+                break;
+            }
+        }
+
+        if (!wasHandled)
+        {
+            result = DefWindowProc(hwnd, message, wParam, lParam);
+        }
+    }
+
+    return result;
+}
